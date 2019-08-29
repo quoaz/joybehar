6383725ad5acf17d9a3e3df9986a97916dfcbca8
McMahon, Ian <ian.mcmahon@delta.com>
2019-08-29 13:37:00 -0400

    panel reconnect stuff, not perfect

--- panel.go
+++ panel.go
@@ -34,33 +34,32 @@ func PanelAgent(port string, dcsAgent dcs.Agent, group controls.Interceptor) *pa
 }
 
 func (a *panelAgent) Receive() {
-	time.Sleep(500 * time.Millisecond)
-	alert.Say("Opening panel")
-	port, err := serial.Open(a.options)
-	if err != nil {
-		alert.Sayf("Error opening serial port: %v", err)
-		alert.Say("Disabling dcsbios panel")
-		return
-	}
-	defer port.Close()
-
-	scanner := bufio.NewScanner(port)
-
-	for scanner.Scan() {
-		msg, err := dcs.DCSMsgFromString(scanner.Text())
+	for {
+		time.Sleep(5000 * time.Millisecond)
+		port, err := serial.Open(a.options)
 		if err != nil {
-			alert.Sayf("%v", err)
 			continue
 		}
-		msg, err = a.group.Intercept(msg)
-		if err != nil {
-			alert.Sayf("%v", err)
-			continue
-		}
+		defer port.Close()
+		alert.Say("Opening panel")
 
-		fmt.Printf("%v\n", msg)
-		a.dcsAgent.SendMsg(msg)
+		scanner := bufio.NewScanner(port)
+
+		for scanner.Scan() {
+			msg, err := dcs.DCSMsgFromString(scanner.Text())
+			if err != nil {
+				alert.Sayf("%v", err)
+				continue
+			}
+			msg, err = a.group.Intercept(msg)
+			if err != nil {
+				alert.Sayf("%v", err)
+				continue
+			}
+
+			fmt.Printf("%v\n", msg)
+			a.dcsAgent.SendMsg(msg)
+		}
+		alert.Say("Panel exited")
 	}
-	alert.Say("Panel receive exiting")
-	go a.Receive()
 }
